6c6a02f2f2cb69c7a231205b6138fc768ce1b07e
Tawi T Jordan <aiseiab05@yahoo.com>
2013-11-15 16:40:42 -0800

    Update Plugwoot.js

--- Autowoot.js/Plugwoot.js
+++ Autowoot.js/Plugwoot.js
@@ -25,13 +25,13 @@
 var path = 'http://pastebin.com/raw.php?i=';
  
  
-function API.chatLog(msg)
+function print(msg)
 {
   $('#chat-messages').append('<div class="chat-update"><span class="chat-text" style="color:#00baff;font-weight:bold">' + msg + '</span></div>');
 }
  
 var scriptFail = window.setTimeout(function() {
-    API.chatLog('@NeonGithub says: Woops an error occurred');
+    print('@NeonGithub says: Woops an error occurred');
   }, 2000);
 
  
