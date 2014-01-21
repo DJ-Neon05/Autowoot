cbc8dda6ae24ee8d8346a921e65299835fdbff5a
Tawi T Jordan <aiseiab05@yahoo.com>
2014-01-20 22:48:49 -0800

    Update Alpha.js

--- Autowoot.js/Alpha.js
+++ Autowoot.js/Alpha.js
@@ -43,10 +43,10 @@ function message(contents) {
 }
  
 var scriptFail = window.setTimeout(function() {
-    admin('Oops! An Error Occurred');
+  message('Oops! An Error Occurred');
   }, 2000);
 
 $.getScript(path + 'd114cGtJ', function() {
  message("version "+ PlugStation.version +" is now available!");
  window.clearTimeout(scriptFail);
-});
+ });
