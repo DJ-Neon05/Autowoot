484a960e2bfdd15148cd86464540c07b4a9e3947
Tawi T Jordan <aiseiab05@yahoo.com>
2014-03-09 22:02:14 -0700

    Update Plugwoot.js

--- Autowoot.js/Plugwoot.js
+++ Autowoot.js/Plugwoot.js
@@ -44,7 +44,7 @@ function message(contents) {
 var scriptFail = window.setTimeout(function() {
   message("Oops! Something went wrong!")
   //message('Sorry plugwoot is updating! :(');
-  }, 1500);
+  }, 2500);
 
  
 $.getScript(path + 'NPqKyJwy', function() {
